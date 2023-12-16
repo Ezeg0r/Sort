631c63c23690db50b76aec0da1be69eec786348b
Егор Ясинсикй <Egormi7@mail.ru>
2023-12-16 20:17:00 +0300

    Всё готово для теста

--- Task2/mergesort/main.cpp
+++ Task2/mergesort/main.cpp
@@ -8,11 +8,11 @@ void mergeSort(vector <int> &a, int l, int r) {
 	if (l >= r) {
 		return;
 	}
-	
+
 	int m = (l + r) / 2;
 	mergeSort(a, l, m);
 	mergeSort(a, m + 1, r);
-	
+
 	vector <int> med;
 	int i = l, j = m + 1;
 	while (i <= m && j <= r) {
@@ -29,7 +29,7 @@ void mergeSort(vector <int> &a, int l, int r) {
 	for (int k = j; k <= r; k++) {
 		med.push_back(a[k]);
 	}
-	
+
 	for (int k = l; k <= r; k++) {
 		a[k] = med[k - l];
 	}
@@ -38,19 +38,19 @@ void mergeSort(vector <int> &a, int l, int r) {
 int main() {
 	ifstream fin("input.txt");
 	ofstream fout("output.txt");
-	
+
 	int n;
 	fin >> n;
-	
+
 	vector <int> a(n);
 	for (int i = 0; i < n; i++) {
 		fin >> a[i];
 	}
-	
+
 	int t0 = clock();
 	mergeSort(a, 0, n - 1);
 	int t = clock();
-	
+
 	fout << t - t0 << endl;
 	return 0;
 }
